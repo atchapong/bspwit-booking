cfc641e54147d87d6fd6a6d094cd21df919329f1
game.p <game.p@kos.co.th>
2023-12-23 21:21:10 +0700

    update

--- index.php
+++ index.php
@@ -34,15 +34,16 @@ $item = new Item();
 
 
     <script>
-        var scheds = getAllBooking();
-        console.log(scheds)
+        var booking_data = getAllBooking();
+        console.log(booking_data)
         console.log(getAllItem())
         document.addEventListener('DOMContentLoaded', () => {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl, {
                 headerToolbar: {
-                    left: 'prev,next today',
-                    right: 'dayGridMonth,list myCustomButton',
+                    left: 'prev,next',
+                    // right: 'dayGridMonth,list myCustomButton today',
+                    right: 'myCustomButton today',
                     center: 'title',
                 },
                 buttonText: {
@@ -116,9 +117,48 @@ $item = new Item();
 
         $(document).ready(() => {
             const item = getAllItem()
-            item.forEach(element => {
-                $('#select-create').append('<option value="'+element.id+'">'+element.name+'</option>')
+            const booking = getAllBooking()
+            
+
+            //show data table all car
+            if (item.length < 1) {
+                setCellsNotData('all-item', '#tr-all-item', 'ไม่มีรถ')
+            } else {
+                item.forEach(element => {
+                $('#select-create').append('<option value="' + element.id + '">' + element.name + '</option>')
+                setRows('all-item', [element.name, element.type_name])
             });
+            }
+
+            //show data table reserv all
+            if (booking.length < 1) {
+                setCellsNotData('reserv-last', '#tr-reserv-last', 'ไม่มีการจองล่าสุด')
+            } else {
+                booking.forEach(element => {
+                    // console.log('element',element)
+                setRows('reserv-last', [element.name, element.fullname, `${element.date_start} ถึง ${element.date_end}` ])
+            });
+            }
+
+            // show data table reserv today
+            const ds = new Date()
+            const de = new Date()
+            ds.setHours(0,0,0,0)
+            de.setHours(23,59,59,59)
+            console.log( new Date(booking[0].date_start) >= ds)
+            console.log(ds)
+
+            console.log(de)
+            const reserv_today = booking.filter(data => new Date(data.date_start) >= ds && new Date(data.date_start) <= de)
+            if (reserv_today.length < 1) {
+                setCellsNotData('reserv-today', '#tr-reserv-today', 'ไม่มีการจองในวันนี้')
+            } else {
+                reserv_today.forEach(element => {
+                console.log('element',element)
+                setRows('reserv-today', [element.name, element.fullname, `${element.date_start} ถึง ${element.date_end}` ])
+            });
+            }
+            
             $('#form-create-booking').submit((e) => {
                 //prevent Default functionality
                 // console.log(e.target)
@@ -128,22 +168,26 @@ $item = new Item();
                 var formData = {
                     fullname: $("#fullnanme").val(),
                     item_id: $("#select-create").val(),
-                    date_start: $("#date-start").val().toISOString().slice(0, 19).replace("T", " "),
-                    date_end:$("#date-end").val().toISOString().slice(0, 19).replace("T", " "),
+                    date_start: new Date($("#date-start").val()),
+                    date_end: new Date($("#date-end").val()),
                 };
 
 
                 const booking_all = getAllBooking();
 
-                console.log(booking_all[0].date_start < formData.date_start)
-                console.log(booking_all[0].date_start);
+                console.log(new Date(booking_all[0].date_start) < formData.date_start)
+                console.log(new Date(booking_all[0].date_start));
                 console.log(formData.date_start);
-                const filter_date_start = booking_all.filter(data => data.date_start >= formData.date_start && data.date_end <= formData.date_end)
-                console.log(filter_date_start);
-                if (filter_date_start.length == 0) {
-                    alert('err')
-                    return 
+                const filter_date_start = booking_all.filter(data => new Date(data.date_start) <= formData.date_start && new Date(data.date_end) >= formData.date_start)
+                console.log('filter_date_start',filter_date_start);
+                const filter_date_end = booking_all.filter(data => new Date(data.date_start) <= formData.date_end && new Date(data.date_end) >= formData.date_end)
+                console.log('filter_date_end',filter_date_end);
+                
+                if (filter_date_start.length > 0 || filter_date_end.length > 0) {
+                    alert('จองไปแล้ว')
+                    return
                 }
+                return
 
 
 
@@ -179,6 +223,28 @@ $item = new Item();
                 $('#cancel-booking').prop('disabled', status);
             }
             return status
+        }
+        const setCellsNotData = (ele_id, tr_id, text) => {
+            
+            const table = document.getElementById(ele_id);
+            var totalRowCellCount = table.rows[0].cells.length; // 5
+            const tr = document.querySelector(tr_id);
+            console.log(tr)
+            tr.innerHTML += `<td colspan="${totalRowCellCount}" class="text-center">${text}</td>`;
+            return
+        }
+        const setRows = (ele_id, data) => {
+            var table = document.getElementById(ele_id);
+
+            var totalRowCellCount = table.rows[0].cells.length; // 5
+            var row = table.insertRow(-1);
+
+            for (let i = 0; i < Number(totalRowCellCount); i++) {
+                     var cell = row.insertCell(i);
+                    cell.innerHTML = data[i];
+            }
+
+            return
         }
     </script>
 </head>
@@ -229,9 +295,72 @@ $item = new Item();
     </div>
 
     <div class=".container-fluid p-3">
-        <div class="container border border-2 p-3 rounded-lg shadow">
-            <div id='calendar'></div>
+        <div class="row justify-content-md-center">
+            <div class="col-9">
+                <div class="container-fluid border border-2 p-3 rounded-lg shadow">
+                    <div id='calendar'></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="container-fluid border border-2 rounded-lg shadow p-2">
+                    <H1 class="text-warning text-center">รายการจองวันนี้</H1>
+                    <table class="table" id="reserv-today">
+                        <thead>
+                            <tr>
+                                <th scope="col">รถ</th>
+                                <th scope="col">ผู้จอง</th>
+                                <th scope="col">วันที่</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr id="tr-reserv-today"></tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="container py-3">
+                </div>
+
+                <div class="container-fluid border border-2 rounded-lg shadow p-2">
+                <H1 class="text-warning text-center">รายการจองล่าสุด</H1>
+                <table class="table" id="reserv-last">
+                        <thead>
+                            <tr>
+                                <th scope="col">รถ</th>
+                                <th scope="col">ผู้จอง</th>
+                                <th scope="col">วันที่</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr id="tr-reserv-last">
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="container py-3">
+                </div>
+
+                <div class="container-fluid border border-2 rounded-lg shadow p-2">
+                <H1 class="text-warning text-center">รถทั้งหมด</H1>
+                <table class="table" id="all-item">
+                        <thead>
+                            <tr>
+                                <th scope="col">ชื่อ</th>
+                                <th scope="col">ประเภท</th>
+                            </tr>
+                        </thead>
+                            <tr id="tr-all-item">
+                            </tr>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
+
+
     </div>
 
     <!-- ==============================Modal===================== -->
@@ -246,7 +375,7 @@ $item = new Item();
                     </button> -->
                 </div>
                 <div class="modal-body">
-                <form id="form-create-booking">
+                    <form id="form-create-booking">
                         <div class="form-group">
                             <label for="fullanme" class="col-form-label">ชื่อ - สกุล :</label>
                             <input type="text" class="form-control" id="fullnanme" name="fullnanme" required>
@@ -265,7 +394,7 @@ $item = new Item();
                             <label for="date-end" class="col-form-label">สิ้นสุด :</label>
                             <input type="datetime-local" class="form-control" id="date-end" required>
                         </div>
-                    
+
                 </div>
                 <div class="modal-footer text-center">
                     <button class="btn btn-primary" type="summit" id="create-booking">
